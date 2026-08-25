1fe005ff9d3d8debe334fd2cbe50e7b5e2444230
Burak Duvakli <70605238+SoofiBD@users.noreply.github.com>
2026-08-25 13:08:36 +0300

    feat(alerts): add spending notifications (#42)
    Persist spending alerts and configurable daily and weekly limits.\n\nCloses #18

--- finance_module/config/routes.rb
+++ finance_module/config/routes.rb
@@ -3,6 +3,10 @@ scope :finance, module: :personal_finance, as: :finance do
   resource :dashboard, only: :show
   resource :spending_report, only: :show
   resource :cash_flow_forecast, only: :show
+  resources :notifications, only: :index do
+    post :mark_all_read, on: :collection
+  end
+  resource :notification_settings, only: :update
   resources :tags, except: :show do
     collection do
       get :report
---
+++ test/controllers/personal_finance/notifications_controller_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class PersonalFinance::NotificationsControllerTest < ActionDispatch::IntegrationTest
+  setup { User.dashboard_owner.update!(onboarded_at: Time.current) }
+
+  test "shows notifications and saves spending limits" do
+    get finance_notifications_path
+    assert_response :success
+
+    patch finance_notification_settings_path, params: {user: {daily_spending_limit: 300, weekly_spending_limit: 1500}}
+    assert_equal 300, User.dashboard_owner.reload.daily_spending_limit.to_f
+    assert_equal 1500, User.dashboard_owner.weekly_spending_limit.to_f
+  end
+end
