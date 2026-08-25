49c859d89ff8be14e6cd5f9d5e9314bbab1c0885
Burak Duvakli <70605238+SoofiBD@users.noreply.github.com>
2026-08-25 13:28:45 +0300

    feat: add smart category suggestions (#46)
    * feat(categories): suggest from notes
    
    Closes #22
    
    * style(routes): format suggestions route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
     resources :exchange_rates, only: %i[index create edit update destroy]
     resources :transactions, except: :show do
       collection do
+        get :category_suggestion
         get :import
         post :import, action: :create_import
         post :preview_import
