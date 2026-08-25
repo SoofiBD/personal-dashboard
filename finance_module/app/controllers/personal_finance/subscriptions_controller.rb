39d65f8b72de8073c6dcd4153be610659a351031
SoofiBD <burakduvakli@hotmail.com>
2026-08-25 13:11:10 +0300

    feat(subscriptions): add tracker
    Persist linked recurring subscriptions and renewal details.\n\nCloses #19

---
+++ finance_module/app/controllers/personal_finance/subscriptions_controller.rb
@@ -0,0 +1,59 @@
+module PersonalFinance
+  class SubscriptionsController < ApplicationController
+    before_action :set_subscription, only: %i[edit update destroy]
+
+    def index
+      @subscriptions = owned(Subscription).includes(:recurring_rule).order(active: :desc, renewal_on: :asc, name: :asc)
+      @monthly_total = @subscriptions.active.sum(&:monthly_cost)
+      @yearly_total = @subscriptions.active.sum(&:yearly_cost)
+      @upcoming_subscriptions = @subscriptions.active.select(&:upcoming_renewal?)
+    end
+
+    def new
+      @subscription = owned(Subscription).new(billing_interval: "monthly", active: true)
+      load_recurring_rules
+    end
+
+    def create
+      @subscription = owned(Subscription).new(subscription_params)
+      if @subscription.save
+        redirect_to finance_subscriptions_path, notice: "Abonelik kaydedildi."
+      else
+        load_recurring_rules
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      load_recurring_rules
+    end
+
+    def update
+      if @subscription.update(subscription_params)
+        redirect_to finance_subscriptions_path, notice: "Abonelik güncellendi."
+      else
+        load_recurring_rules
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @subscription.destroy!
+      redirect_to finance_subscriptions_path, notice: "Abonelik silindi."
+    end
+
+    private
+
+    def set_subscription
+      @subscription = owned(Subscription).find(params[:id])
+    end
+
+    def load_recurring_rules
+      @recurring_rules = owned(RecurringRule).active.expense.order(:note, :amount)
+    end
+
+    def subscription_params
+      params.require(:subscription).permit(:name, :amount, :billing_interval, :renewal_on, :last_used_on, :active, :recurring_rule_id)
+    end
+  end
+end
